b68f82c6385cb506c17ea16221852449a6d71045
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-05-03 09:34:54 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){ 
 
 
-	$(document).on('click','.mobile_bars',function(){
-		$('.header_menu').slideToggle();
-	});
-
-	// var count = 0;
-
 	// $(document).on('click','.mobile_bars',function(){
-	// 	if(count == 0){ 
-	// 		$('.header_menu').show();
-	// 		$('.banner').css({'z-index':'-1'});
-	// 		$(this).html('<i class="fa-solid fa-xmark"></i>');
-	// 		count = 1;
-	// 	}else{
-	// 		$('.header_menu').hide();
-	// 		$('.banner').css({'z-index':'99'});
-	// 		$(this).html('<i class="fas fa-bars"></i>');
-	// 		count = 0;
-	// 	}
-	// })
+	// 	$('.header_menu').slideToggle();
+	// });
+
+	var count = 0;
+
+	$(document).on('click','.mobile_bars',function(){
+		if(count == 0){ 
+			$('.header_menu').show();
+			$('.banner').css({'z-index':'-1'});
+			$(this).html('<i class="fa-solid fa-xmark"></i>');
+			count = 1;
+		}else{
+			$('.header_menu').hide();
+			$('.banner').css({'z-index':'99'});
+			$(this).html('<i class="fas fa-bars"></i>');
+			count = 0;
+		}
+	})
 
 
 
